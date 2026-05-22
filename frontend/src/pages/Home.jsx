143dab21a56b4455d9048745d91d62ca980cf7fa
Your Namezm-rafi <zrafi221217@bscse.uiu.ac.bd>
2026-05-22 22:25:27 +0600

    chat

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
-import Chatbot from './chatbot'
+
 const Home = () => {
   return (
     <div>
